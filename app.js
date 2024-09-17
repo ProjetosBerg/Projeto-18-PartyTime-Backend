118580950083dda188781893b581af6bb400aea1
Bergkley <bergkley@gmail.com>
2024-09-17 09:36:20 -0300

    feat: Projeto 18 partyTime backend

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const cors = require("cors");
+require('dotenv').config();
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+// DB connection
+
+const conn = require('./db/conn');
+
+// Routes
+
+const routes = require('./routes/router');
+app.use("/api", routes);
+
+conn();
+
+const Port = process.env.PORT || 3000;
+
+app.listen(Port, function(){
+    console.log("Servidor online")
+})
---
+++ db/conn.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+require('dotenv').config();
+
+async function main(){
+    try{
+        mongoose.set('strictQuery', true);
+        
+        const username = process.env.DB_USERNAME;
+        const password = process.env.DB_PASSWORD;
+        const uri = `mongodb+srv://${username}:${password}@cluster0.wgrog.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+        await mongoose.connect(uri);
+
+        console.log('MongoDB conectado com sucesso!');
+    } catch(error) {
+        console.log(error);
+    }
+}
+
+module.exports = main;
